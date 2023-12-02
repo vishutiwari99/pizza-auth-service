e52578281213c4bbbd695028b8368b739adc7003
Vaibhav Tiwari <vaibhav.tiwari@unilever.com>
2023-12-02 18:12:41 +0530

    added lint-staged

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 function login(username: string): string {
-  return username;
+  const user = {
+    name: 'vishu',
+  };
+  const nickName = user.name;
+  return username + nickName;
 }
 
 login('vaibhav');
