c662b13cd24b361a805ecbfde64d6f31a6e90af9
Vaibhav Tiwari <vaibhav.tiwari@unilever.com>
2023-12-03 13:51:45 +0530

    added logger

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
 import app from './app';
 import { Config } from './config';
+import logger from './config/logger';
 
 const startServer = () => {
   const PORT = Config.PORT;
   try {
-    // eslint-disable-next-line no-console, @typescript-eslint/no-unsafe-call
-    app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error(error);
-    process.exit(1);
+    app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
+  } catch (err) {
+    if (err instanceof Error) {
+      logger.error(err.message);
+      setTimeout(() => {
+        process.exit(1);
+      }, 1000);
+    }
   }
 };
 
