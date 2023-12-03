5ffd6e522e5225e30eed7b68c4192da590221184
Vaibhav Tiwari <vaibhav.tiwari@unilever.com>
2023-12-03 14:28:02 +0530

    Added Error handler

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,26 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import { HttpError } from 'http-errors';
+import logger from './config/logger';
 const app = express();
 
-app.get('/', (req, res): void => {
+app.get('/', async (req, res) => {
   res.send('Welcome to Auth Service');
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.message);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    errors: [
+      {
+        type: err.name,
+        msg: err.message,
+        path: '',
+        location: '',
+      },
+    ],
+  });
 });
 
 export default app;
