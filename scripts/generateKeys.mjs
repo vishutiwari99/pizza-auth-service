6bc85d593f840583b0db7bf7efc1459199df8e00
Vaibhav Tiwari <vishutiwari99@gmail.com>
2024-01-11 06:16:04 +0530

    Added key generation script

--- scripts/generateKeys.mjs
+++ scripts/generateKeys.mjs
@@ -1,20 +1,20 @@
 import crypto from 'crypto';
 import fs from 'fs';
 
-const {publicKey,privateKey} = crypto.generateKeyPairSync('rsa',{
-    modulusLength:2048,
-    publicKeyEncoding:{
-        type:'pkcs1',
-        format:'pem'
-    },
-    privateKeyEncoding:{
-         type:'pkcs1',
-         format:'pem'
-    }
-})
+const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
+  modulusLength: 2048,
+  publicKeyEncoding: {
+    type: 'pkcs1',
+    format: 'pem',
+  },
+  privateKeyEncoding: {
+    type: 'pkcs1',
+    format: 'pem',
+  },
+});
 
-console.log("Public Key", publicKey);
-console.log("Private Key", privateKey);
+console.log('Public Key', publicKey);
+console.log('Private Key', privateKey);
 
-fs.writeFileSync('certs/private.pem',privateKey);
-fs.writeFileSync('certs/public.pem',publicKey);
+fs.writeFileSync('certs/private.pem', privateKey);
+fs.writeFileSync('certs/public.pem', publicKey);
