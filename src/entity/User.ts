ff41d3efb6905a7b7b9ae8a3433c55d0cb57f951
Vaibhav Tiwari <vishutiwari99@gmail.com>
2024-01-25 22:13:34 +0530

    Added FK in user

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Tenant } from './Tenant';
 
 @Entity({ name: 'users' })
 export class User {
@@ -16,4 +17,7 @@ export class User {
 
   @Column()
   role: string;
+
+  @ManyToOne(() => Tenant)
+  tenant: Tenant;
 }
