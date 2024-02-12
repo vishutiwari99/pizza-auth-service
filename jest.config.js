7ca2fed086647686229202dae0d6e3a7b52b5542
Vaibhav Tiwari <vishutiwari99@gmail.com>
2024-02-12 06:36:58 +0530

    Jest config and package json config change

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  verbose:true,
-  collectCoverage:true,
-  coverageProvider:'v8',
-  collectCoverageFrom:[
-    "src/**/*.ts",
-    "!tests/**",
-    "!**/node_modules/**"
-  ]
+  verbose: true,
+  collectCoverage: true,
+  coverageProvider: 'v8',
+  collectCoverageFrom: ['src/**/*.ts', '!tests/**', '!**/node_modules/**'],
 };
